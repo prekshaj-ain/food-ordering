f2cb0f92caa61f853d9d71ed87c3ab8e001ad2e4
preksha <jainpreksha2712@gmail.com>
2023-10-20 09:42:40 +0530

    :new: new: add endpoints for homepage api and image api

---
+++ src/Constants/endPoints.js
@@ -0,0 +1,6 @@
+export const HOMEPAGE_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7009403&lng=77.2721047&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const IMAGE_API = function (id) {
+  return `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${id}`;
+};
