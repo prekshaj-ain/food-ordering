47e13ec1bc64e41dc79fde05ff94507393dafe18
preksha <jainpreksha2712@gmail.com>
2023-10-21 23:05:26 +0530

    :up: add distance in restaurant card

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,4 +1,6 @@
 import StarsIcon from "@mui/icons-material/Stars";
+import { Link } from "react-router-dom";
+
 import { IMAGE_API } from "../Constants/endPoints";
 import styles from "./RestaurantCard.module.css";
 
@@ -7,20 +9,23 @@ const RestaurantCard = function ({ details }) {
     details.aggregatedDiscountInfoV3 &&
     Object.values(details.aggregatedDiscountInfoV3).slice(0, 2);
   let {
+    id,
     areaName,
     avgRatingString: rate,
     cuisines,
     cloudinaryImageId: imgId,
     name,
+    sla,
   } = details;
   let image = IMAGE_API(imgId);
   return (
-    <div className={styles.card}>
+    <Link to={`restaurants/${id}`} className={styles.card}>
       <div className={styles.image}>
         <img src={image} alt={name} />
         {offer && (
           <h6>
-            <span>{offer[0]}</span> <span>{offer[1]}</span>
+            <span>{offer[0]} </span>
+            <span>{offer[1]}</span>
           </h6>
         )}
       </div>
@@ -28,12 +33,14 @@ const RestaurantCard = function ({ details }) {
         <h5>{name}</h5>
         <div className={styles.rate}>
           <StarsIcon color="success" fontSize="5px" />
-          <h5>{rate}</h5>
+          <h6>
+            {rate} · {sla.slaString}
+          </h6>
         </div>
         <p>{cuisines.join(", ")}</p>
         <p>{areaName}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
