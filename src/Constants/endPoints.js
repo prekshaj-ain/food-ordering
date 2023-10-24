dd68c7f90b1ed60c80f3737d9f1f92523d31e843
preksha <jainpreksha2712@gmail.com>
2023-10-24 15:13:48 +0530

    :up: add resraurant details end point

--- src/Constants/endPoints.js
+++ src/Constants/endPoints.js
@@ -4,3 +4,7 @@ export const HOMEPAGE_API =
 export const IMAGE_API = function (id) {
   return `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${id}`;
 };
+
+export const RESTAURANT_DETAILS = function (id) {
+  return `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7009403&lng=77.2721047&restaurantId=${id}`;
+};
