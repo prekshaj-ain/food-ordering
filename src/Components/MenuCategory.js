cf1c738561042b4a4cc9705160968ad516727352
preksha <jainpreksha2712@gmail.com>
2023-10-25 13:18:10 +0530

    :up: add recursive component

--- src/Components/MenuCategory.js
+++ src/Components/MenuCategory.js
@@ -1,18 +1,48 @@
+import { useState } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
 import styles from "./MenuCategory.module.css";
 import MenuItem from "./MenuItem";
 
-const MenuCategory = function ({ category }) {
-  // console.log(category);
+const MenuCategory = function ({ category, subCat }) {
   console.log(category);
-  const { title, itemCards: items } = category;
+  const { title, itemCards: items, categories } = category;
+  const [show, setShow] = useState(true);
+  const handleClick = function () {
+    setShow((prev) => !prev);
+  };
+  if (categories) {
+    return (
+      <div>
+        <div className={styles.category}>
+          <button className={styles.toggleShow} onClick={handleClick}>
+            <h6>{`${title}  (${categories?.length})`}</h6>
+            <span className={!show ? styles.icon : ""}>
+              <KeyboardArrowUpIcon fontSize="small" />
+            </span>
+          </button>
+          {show &&
+            categories?.map((cat, index) => {
+              return <MenuCategory key={index} category={cat} subCat />;
+            })}
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       {items && (
-        <div className={styles.category}>
-          <h6>{`${title}  (${items?.length})`}</h6>
-          {items?.map((item, index) => {
-            return <MenuItem key={index} item={item} />;
-          })}
+        <div className={subCat ? "" : styles.category}>
+          <button className={styles.toggleShow} onClick={handleClick}>
+            <h6>{`${title}  (${items?.length})`}</h6>
+            <span className={!show ? styles.icon : ""}>
+              <KeyboardArrowUpIcon fontSize="small" />
+            </span>
+          </button>
+          {show &&
+            items?.map((item, index) => {
+              return <MenuItem key={index} item={item} />;
+            })}
         </div>
       )}
     </div>
