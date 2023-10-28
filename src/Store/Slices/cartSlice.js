b2d9433f1a15ba2dee9c86163207d081edd16a49
preksha <jainpreksha2712@gmail.com>
2023-10-28 21:31:33 +0530

    :up: add subTotal field in cartSlice

--- src/Store/Slices/cartSlice.js
+++ src/Store/Slices/cartSlice.js
@@ -6,31 +6,36 @@ const cartSlice = createSlice({
     restaurant: null,
     items: [],
     quantities: [],
+    subTotal: 0,
   },
   reducers: {
     addItem: (state, action) => {
       state.restaurant = action.payload.restaurant;
       state.items.push(action.payload.item);
       state.quantities.push(action.payload.quantity);
+      state.subTotal += action.payload.item.price * action.payload.quantity;
     },
     updateQuantity: (state, action) => {
-      let index = items.find((item) => item === action.payload.item);
+      let index = state.items.findIndex((item) => item.id == action.payload.id);
       if (index !== -1) {
         state.quantities[index] = action.payload.quantity;
+        state.subTotal = state.items.reduce((total, item, index) => {
+          return total + item.price * state.quantities[index];
+        }, 0);
       }
     },
     removeItem: (state, action) => {
-      let index = items.find((item) => item === action.payload);
+      let index = state.items.findIndex((item) => item.id == action.payload);
       if (index !== -1) {
         state.items.splice(index, 1);
         state.quantities.splice(index, 1);
       }
+      if (state.items.length == 0) state.restaurant = null;
     },
     clearCart: (state) => {
       state.items = [];
       state.quantities = [];
       state.restaurant = null;
-      state.deliveryCharge = 0;
     },
   },
 });
