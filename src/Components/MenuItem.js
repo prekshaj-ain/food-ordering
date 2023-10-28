ea1614e2fc10b008404f1bf4ae7e5411b19735cd
preksha <jainpreksha2712@gmail.com>
2023-10-28 21:34:37 +0530

    :up: add buttons to update quantity

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,29 +1,54 @@
 import StarIcon from "@mui/icons-material/Star";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import VEG from "../Constants/Assets/veg.png";
 import NONVEG from "../Constants/Assets/nonveg.png";
 import { IMAGE_API } from "../Constants/endPoints";
 import styles from "./MenuItem.module.css";
-import { addItem } from "../Store/Slices/cartSlice";
+import { addItem, removeItem, updateQuantity } from "../Store/Slices/cartSlice";
 
 const MenuItem = function ({ item }) {
   const dispatch = useDispatch();
   const currentRestaurant = useSelector((store) => store.app.currentRestaurant);
   const cartRestaurant = useSelector((store) => store.cart.restaurant);
+  const quantities = useSelector((store) => store.cart.quantities);
+  const items = useSelector((store) => store.cart.items);
   let { info } = item?.card;
-  let { isVeg, isBestseller, price, name, description, imageId } = info;
+  let {
+    isVeg,
+    isBestseller,
+    price,
+    defaultPrice,
+    name,
+    description,
+    imageId,
+    id,
+  } = info;
 
+  const index = items.findIndex((item) => item.id === id);
   const handleClick = function () {
     if (cartRestaurant === null || currentRestaurant.id === cartRestaurant.id) {
       const details = {
         restaurant: currentRestaurant,
-        item: { isVeg, name, price: price / 100 },
+        item: { id, isVeg, name, price: price / 100 || defaultPrice / 100 },
         quantity: 1,
       };
       dispatch(addItem(details));
     } else {
       alert("you can add only from one restaurant");
+    }
+  };
+  const handleIncrement = function () {
+    let currQuantity = quantities[index];
+    dispatch(updateQuantity({ id, quantity: currQuantity + 1 }));
+  };
+  const handleDecrement = function () {
+    let currQuantity = quantities[index];
+    if (currQuantity - 1 == 0) {
+      dispatch(removeItem(id));
+    } else {
+      dispatch(updateQuantity({ id, quantity: currQuantity - 1 }));
     }
   };
 
@@ -43,14 +68,22 @@ const MenuItem = function ({ item }) {
             )}
           </div>
           <div>{name}</div>
-          <p>₹ {price / 100}</p>
+          <p>₹ {price / 100 || defaultPrice / 100}</p>
           <div className={styles.description}>{description}</div>
         </div>
         <div className={styles.image}>
           <img src={IMAGE_API(imageId)} alt={name} />
-          <button className={styles.add} onClick={handleClick}>
-            ADD
-          </button>
+          {quantities[index] > 0 ? (
+            <div className={`${styles.add} ${styles.quantity}`}>
+              <button onClick={handleDecrement}>−</button>
+              <span>{quantities[index]}</span>
+              <button onClick={handleIncrement}>+</button>
+            </div>
+          ) : (
+            <button className={styles.add} onClick={handleClick}>
+              ADD
+            </button>
+          )}
         </div>
       </div>
       <div className={styles.divider}></div>
