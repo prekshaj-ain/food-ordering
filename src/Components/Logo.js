ea3961a09cdca86b25590774ec9e1965c9c80aae
preksha <jainpreksha2712@gmail.com>
2023-10-28 21:29:09 +0530

    :up: add logo to app

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,5 +1,14 @@
+import LOGO from "../Constants/Assets/food-delivery.png";
+
 const Logo = function () {
-  return <h3>Eat</h3>;
+  return (
+    <div style={{ height: "50px", width: "50px" }}>
+      <img
+        src={LOGO}
+        style={{ objectFit: "cover", height: "100%", width: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default Logo;
