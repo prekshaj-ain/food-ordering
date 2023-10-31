a1851ad8a6b7ec3dd084ecb31bcb1b0c41681b2a
preksha <jainpreksha2712@gmail.com>
2023-10-31 16:13:55 +0530

    :up: add footer show cart total price and items count

--- src/Pages/SingleRestaurant.js
+++ src/Pages/SingleRestaurant.js
@@ -1,14 +1,18 @@
-import { useParams } from "react-router-dom";
-import Layout from "../Components/Layout";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+
+import Layout from "../Components/Layout";
 import { RESTAURANT_DETAILS } from "../Constants/endPoints";
 import RestaurantDetails from "../Components/RestaurantDetails";
 import Skeleton from "../Components/Skeleton";
+import styles from "./SingleRestaurant.module.css";
 
 const SingleRestaurant = function () {
+  const subTotal = useSelector((store) => store.cart.subTotal);
+  const items = useSelector((store) => store.cart.items);
   let { id } = useParams();
   let [details, setDetails] = useState();
-  let [menu, setMenu] = useState();
   useEffect(() => {
     fetchRestaurant();
     async function fetchRestaurant() {
@@ -25,8 +29,18 @@ const SingleRestaurant = function () {
       <RestaurantDetails
         info={details[0]}
         offers={details[1]}
-        menu={details[2].groupedCard}
+        menu={details.at(-1).groupedCard}
       />
+      {items.length > 0 && (
+        <footer className={styles.cartFooter}>
+          <div>
+            <span>
+              {items.length} Items | ₹{subTotal}
+            </span>
+            <Link to="/checkout">VIEW CART</Link>
+          </div>
+        </footer>
+      )}
     </Layout>
   );
 };
