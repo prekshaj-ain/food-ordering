0d44ce8a219c6ad0c298eb32d01534dc5fb5bc04
preksha <jainpreksha2712@gmail.com>
2023-11-01 17:46:59 +0530

    :up: change home page skeleton

--- src/Components/Skeleton.js
+++ src/Components/Skeleton.js
@@ -1,18 +1,15 @@
 import Layout from "./Layout";
 import styles from "./Skeleton.module.css";
 const Skeleton = function ({ type }) {
-  if (type === "Home") {
+  if (type === "Posts") {
     return (
-      <Layout>
-        <div className={styles.container}>
-          <div className={styles.heading}></div>
-          <div className={styles.cards}>
-            {Array.from(Array(10)).map((e, index) => {
-              return <div className={styles.card} key={index}></div>;
-            })}
-          </div>
+      <div className={styles.container}>
+        <div className={styles.cards}>
+          {Array.from(Array(10)).map((e, index) => {
+            return <div className={styles.card} key={index}></div>;
+          })}
         </div>
-      </Layout>
+      </div>
     );
   }
   if (type === "Single") {
