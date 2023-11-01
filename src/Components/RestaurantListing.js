19a990b1f9274e92f772a23eb9fcc0a872f4115f
preksha <jainpreksha2712@gmail.com>
2023-11-01 17:50:16 +0530

    :up: show restaurants and loading state

--- src/Components/RestaurantListing.js
+++ src/Components/RestaurantListing.js
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux";
 import AllRestaurants from "./AllRestaurants";
-const RestaurantListing = function ({ restaurants }) {
-  console.log(restaurants);
+import Skeleton from "./Skeleton";
+const RestaurantListing = function () {
+  const restaurants = useSelector((store) => store.restaurants.data);
+  const loading = useSelector((store) => store.restaurants.loading);
+  if (restaurants.length == 0) {
+    return <Skeleton type="Posts" />;
+  }
   return (
     <div style={{ paddingInline: ".5rem" }}>
       <h4 style={{ paddingBlock: "1rem" }}>
         Restaurants with online food delivery in Delhi
       </h4>
       <AllRestaurants list={restaurants} />
+      {loading && <Skeleton type="Posts" />}
     </div>
   );
 };
