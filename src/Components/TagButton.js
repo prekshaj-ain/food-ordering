f91da3417f82f3d88c0e8b7167bed3f59951ad50
preksha <jainpreksha2712@gmail.com>
2023-11-03 14:13:17 +0530

    :up: update style of tag button on active

--- src/Components/TagButton.js
+++ src/Components/TagButton.js
@@ -1,18 +1,24 @@
 const TagButton = function (props) {
+  const buttonStyle = {
+    border: ".5px solid #9999993d",
+    borderRadius: "30px",
+    padding: "5px 10px",
+    display: "flex",
+    alignItems: "center",
+    gap: "5px",
+    width: "fit-content",
+    cursor: "pointer",
+    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+  };
+  const active = {
+    borderColor: "black",
+    backgroundColor: "rgba(99, 99, 99, 0.1)",
+    boxShadow: "none",
+  };
   return (
     <div
       onClick={props.onClick}
-      style={{
-        border: ".5px solid #9999993d",
-        borderRadius: "30px",
-        padding: "5px 10px",
-        display: "flex",
-        alignItems: "center",
-        gap: "5px",
-        width: "fit-content",
-        cursor: "pointer",
-        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
-      }}
+      style={props.active ? { ...buttonStyle, ...active } : buttonStyle}
     >
       {props.children}
     </div>
