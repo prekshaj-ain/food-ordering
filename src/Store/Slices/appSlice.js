29b4a7f1df0fe27688d335888c8d5f7a03f16d86
preksha <jainpreksha2712@gmail.com>
2023-11-03 14:12:29 +0530

    :up: add filter data

--- src/Store/Slices/appSlice.js
+++ src/Store/Slices/appSlice.js
@@ -4,6 +4,38 @@ const appSlice = createSlice({
   name: "app",
   initialState: {
     currentRestaurant: null,
+    filters: {
+      Sort: {
+        type: "radio",
+        subFilter: [
+          { title: "Relevance" },
+          { title: "Delivery Time" },
+          { title: "Rating" },
+          { title: "Cost: high to low" },
+          { title: "Cost: low to high" },
+        ],
+      },
+      Ratings: {
+        type: "checkbox",
+        subFilter: [
+          { title: "Rating 4.5+" },
+          { title: "Rating 4.0+" },
+          { title: "Rating 3.5+" },
+        ],
+      },
+      "Veg/Non-Veg": {
+        type: "radio",
+        subFilter: [{ title: "Veg" }, { title: "Non Veg" }],
+      },
+      "Cost for two": {
+        type: "radio",
+        subFilter: [
+          { title: "Rs. 300-Rs. 600" },
+          { title: "Greater than Rs. 600" },
+          { title: "Less than Rs. 300" },
+        ],
+      },
+    },
   },
   reducers: {
     setCurrentRestaurant: (state, action) => {
