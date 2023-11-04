fc61e73e5a57d923be7dbf0dfda2a45586760fa9
preksha <jainpreksha2712@gmail.com>
2023-11-04 14:17:33 +0530

    :up: use the filtering selector

--- src/Components/RestaurantListing.js
+++ src/Components/RestaurantListing.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
+
 import AllRestaurants from "./AllRestaurants";
 import Skeleton from "./Skeleton";
+import FilterHeader from "./FilterHeader";
+import FiltersModal from "./FiltersModal";
+import { selectFilteredRestaurants } from "../Store/Selectors/filterData";
+
 const RestaurantListing = function () {
-  const restaurants = useSelector((store) => store.restaurants.data);
+  const restaurants = useSelector(selectFilteredRestaurants);
   const loading = useSelector((store) => store.restaurants.loading);
+  const [showFiltersModal, setShowFiltersModal] = useState(false);
   if (restaurants.length == 0) {
     return <Skeleton type="Posts" />;
   }
   return (
-    <div style={{ paddingInline: ".5rem" }}>
-      <h4 style={{ paddingBlock: "1rem" }}>
-        Restaurants with online food delivery in Delhi
-      </h4>
+    <div
+      style={{
+        padding: ".5rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: ".5rem",
+      }}
+    >
+      <h4>Restaurants with online food delivery in Delhi</h4>
+      <FilterHeader setShow={setShowFiltersModal} />
+      {showFiltersModal && <FiltersModal setShow={setShowFiltersModal} />}
       <AllRestaurants list={restaurants} />
       {loading && <Skeleton type="Posts" />}
     </div>
