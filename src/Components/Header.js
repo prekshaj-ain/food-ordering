055d0c75c63c36c2f41f653420cdde77b4b0cf60
preksha <jainpreksha2712@gmail.com>
2023-11-06 22:47:19 +0530

    :up: update links rremove offers and update cart logo with count

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PercentIcon from "@mui/icons-material/Percent";
+import { Badge } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import Logo from "./Logo";
 import styles from "./Header.module.css";
 
 const Header = function () {
+  const items = useSelector((store) => store.cart.items);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.left}>
+        <Link to="/" className={styles.left}>
           <Logo />
-        </div>
+        </Link>
         <div className={styles["header-items"]}>
-          <button className={styles.item}>
-            <SearchIcon fontSize="small" />
-            <h6>Search</h6>
-          </button>
-          <button className={`${styles.item} ${styles.offers}`}>
-            <PercentIcon fontSize="small" />
-            <h6>Offers</h6>
-          </button>
-          <button className={styles.item} tabIndex={0}>
-            <ShoppingCartIcon fontSize="small" />
+          <Link to="/search" className={styles.item}>
+              <SearchIcon fontSize="small" />
+              <h6>Search</h6>
+          </Link>
+          <Link to="/checkout" className={styles.item}>
+            <Badge
+              badgeContent={items.length}
+              color="success"
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+            >
+              <ShoppingCartIcon fontSize="small" />
+            </Badge>
             <h6>Cart</h6>
-          </button>
+          </Link>
         </div>
       </div>
     </div>
