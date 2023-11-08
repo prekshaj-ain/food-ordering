36459560e136d76aa4f390d00f53b72b2a504e45
preksha <jainpreksha2712@gmail.com>
2023-11-08 12:41:17 +0530

    :up: import searchSlice & suggestionsSlice

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,13 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import cartSlice from "./Slices/cartSlice";
 import appSlice from "./Slices/appSlice";
 import restaurantsSlice from "./Slices/restaurantsSlice";
+import suggestionsSlice from "./Slices/suggestionsSlice";
+import searchSlice from "./Slices/searchSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
     cart: cartSlice,
     restaurants: restaurantsSlice,
+    suggestions: suggestionsSlice,
+    search: searchSlice,
   },
 });
 
