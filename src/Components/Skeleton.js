cffd5dc9fb2007aaa997f4e0d5afd505a2a448ab
preksha <jainpreksha2712@gmail.com>
2023-11-10 20:45:12 +0530

    :up: add skeleton for search results loading

--- src/Components/Skeleton.js
+++ src/Components/Skeleton.js
@@ -36,6 +36,22 @@ const Skeleton = function ({ type }) {
       </Layout>
     );
   }
+  if (type === "Search") {
+    return (
+      <div className={styles.box}>
+        {Array.from(Array(5)).map((e, index) => {
+          return (
+            <div key={index} className={styles.content}>
+              <div style={{ width: "20px" }}></div>
+              <div style={{ width: "50%" }}></div>
+              <div style={{ width: "40%" }}></div>
+              <div style={{ width: "30%" }}></div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 export default Skeleton;
