d423cde86032edb7e667f29a9529851a8428aa5f
preksha <jainpreksha2712@gmail.com>
2023-11-17 20:59:47 +0530

    :up: show offline message when connection is lost

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Provider } from "react-redux";
 
 import Header from "./Components/Header";
 import store from "./Store/store";
+import useConnection from "./Store/Hooks/useConnection";
+import "./App.css";
 
 const App = function () {
+  const isOffline = useConnection(false);
   return (
     <Provider store={store}>
       <Header />
       <Outlet />
+      {isOffline && (
+        <div className="connectionModal">
+          <span>Connection Error</span>
+          <span>Please check your internet connection and try again.</span>
+        </div>
+      )}
     </Provider>
   );
 };
