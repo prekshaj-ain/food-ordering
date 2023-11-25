a863f5724946c5c1bbe50327f6447a9741d72801
preksha <jainpreksha2712@gmail.com>
2023-11-25 17:39:38 +0530

    :up: update all the end points to handle cors

--- src/Constants/endPoints.js
+++ src/Constants/endPoints.js
@@ -1,13 +1,13 @@
 export const HOMEPAGE_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7009403&lng=77.2721047&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7009403&lng=77.2721047&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const IMAGE_API = function (id) {
   return `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${id}`;
 };
 
 export const RESTAURANT_DETAILS = function (id) {
-  return `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7009403&lng=77.2721047&restaurantId=${id}`;
+  return `https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7009403&lng=77.2721047&restaurantId=${id}`;
 };
 
 export const SEARCH_SUGGESTIONS =
-  "https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.7009403&lng=77.2721047&trackingId=null&str=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.7009403&lng=77.2721047&trackingId=null&str=";
